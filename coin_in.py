9816e95224517564a278944f7b42b8d745151045
bossware14 <bossware14@gmail.com>
2025-02-11 04:43:43 +0700

    Update coin_in.py

--- coin_in.py
+++ coin_in.py
@@ -7,20 +7,35 @@ from signal import pause
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(12,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-counter = 0
+
 time_start = time.time()
 time_end = time.time()
+counter = 0
 status_gpi = 0
 MONEY = 0
 
 def sensor_callback(channel):
-    global counter,MONEY
+    global counter,MONEY,status_gpi,time_start,time_end
     checkGPOI = GPIO.input(channel)
+    time_start = round(time.time(),1)
+    if checkGPOI == 1 :
+       time_start = round(time.time(),1)
+       status_gpi = checkGPOI
+       MONEY =0
+       counter =0
     if checkGPOI == 0 :
-       counter = counter + 1
-       MONEY = MONEY + 1
-       print('IN',MONEY)
+       time_end = round(time.time(),1)
+       status_gpi = checkGPOI
+       checktime = round(time_end-time_start,1)
 
-GPIO.add_event_detect(12,GPIO.BOTH,callback=sensor_callback)
+    if checkGPOI == 0 and status_gpi == 0 and checktime == 0:
+       MONEY = MONEY +10
+       counter = counter +1
+       status_gpi = 1
+    if MONEY >= 20 :
+       print(MONEY)
+
+#GPIO.add_event_detect(12,GPIO.BOTH,callback=sensor_callback)
+GPIO.add_event_detect(12,GPIO.FALLING,callback=sensor_callback)
 print("Start")
 pause()
